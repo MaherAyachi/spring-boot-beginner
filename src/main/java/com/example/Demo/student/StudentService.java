97725a9d1a3fe02372a29937e99d3df663d362bb
Maher <maherayachi0@gmail.com>
2024-02-01 17:08:16 +0100

    Building the first sample API

--- src/main/java/com/example/Demo/student/StudentService.java
+++ src/main/java/com/example/Demo/student/StudentService.java
@@ -6,13 +6,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Service
-public class StudentService {
-    public List<Student> findAllStudents() {
-        return List.of(
-                new Student("Maher", "Ayachi", LocalDate.now(), "contact@maher.com", 22),
-                new Student("ali", "Ayach", LocalDate.now(), "contact@maher.com", 21),
-                new Student("houssem", "Ayachi", LocalDate.now(), "contact@maher.com", 20)
-
-        );
+public interface StudentService {
+    Student save(Student s);
+   List<Student> findAllStudents();
+   Student findByEmail(String email);
+   Student update(Student s);
+   void delete(String email);
     }
-}
+
---
+++ src/main/java/com/example/Demo/student/inMemoryStudentService.java
@@ -0,0 +1,39 @@
+package com.example.Demo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+@Service
+public class inMemoryStudentService implements StudentService {
+    private final inMemoryStudentDao dao;
+
+    public inMemoryStudentService(inMemoryStudentDao dao){
+        this.dao=dao;
+    }
+    @Override
+    public Student save(Student s){
+        return dao.save(s);
+    }
+
+    @Override
+    public List<Student> findAllStudents() {
+        return null;
+    }
+
+    @Override
+    public Student findByEmail(String email){
+        return dao.findByEmail(email);
+
+    }
+    @Override
+    public Student update (Student s){
+        return dao.update(s);
+
+    }
+    @Override
+    public void delete (String email){
+        dao.delete(email);
+    }
+
+}
